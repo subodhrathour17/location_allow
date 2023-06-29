0ec5a72ed0f804c032cce8ca61b68a4f609eee47
subu <subodhrathour17@gmail.com>
2023-06-29 17:02:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getLocation=()=>{
+    if('geolocation' in navigator){
+        navigator.geolocation.getCurrentPosition(success,error)
+    }
+}
+const success=(position)=>{
+    console.log(position)
+}
+const error = ()=>{
+    console.log("sorry");
+}
+getLocation()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Please allow to Get Your Location</h1>
     </div>
   );
 }
